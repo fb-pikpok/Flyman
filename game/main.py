0baa5bfb2fcab7346665e4ccbef0eecdb091dacc
Florian Böhm <fbohm@pikpok.com>
2025-09-20 16:52:51 +0200

    jumping and gravity

--- game/main.py
+++ game/main.py
@@ -11,26 +11,45 @@ async def main():
     pixel_font = pygame.font.Font("game/assets/fonts/pixeltype.ttf", 50)
     sky_surface = pygame.image.load("game/assets/graphics/sky.png").convert()
 
-    # Player for now is just a circle
-    player_surface = pygame.Surface((50, 50), pygame.SRCALPHA)
-    pygame.draw.circle(player_surface, 'blue', (25, 25), 10)
-    player_x_pos = 600
+
+    player_x_pos = 80
+    player_y_pos = 250
+    player_gravity = 0
+    player_surf = pygame.image.load("game/assets/graphics/player.png").convert_alpha()
+    player_rect = player_surf.get_rect(center = (player_x_pos, player_y_pos))
+
+
+
 
 
     while running:
         for e in pygame.event.get():
             if e.type == pygame.QUIT:
                 running = False
-        # ... update & draw ...
+            
+            # Player logic
+            if e.type == pygame.KEYDOWN:
+                if e.key == pygame.K_SPACE:
+                    player_gravity = -14
 
+        # Background
         screen.blit(sky_surface, (0, 0))
         screen.blit(pixel_font.render("Flyman", True, 'black'), (50, 50))
 
 
-        player_x_pos += 4
-        if player_x_pos > 870:
-            player_x_pos = -30
-        screen.blit(player_surface, (player_x_pos, 250))
+        # Player
+        player_gravity += 0.5
+        player_y_pos += player_gravity
+        screen.blit(player_surf, player_rect)
+        
+        player_rect.y = player_y_pos
+        if player_rect.bottom >= 500:
+            player_rect.bottom = 500
+            player_gravity = 0
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE]:
+            print("UP")
 
 
         pygame.display.flip()
